4246888999e5a2dded10ff4550edb54544ddd11e
IDRISS ABAKAR NOKOUR <nokoury@MacBook-Pro-von-IDRISS.local>
2024-09-23 09:05:24 +0200

    ajout

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -1,6 +1,7 @@
 package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
+import re.forestier.edu.rpg.UpdatePlayer;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -16,6 +17,15 @@ public class UnitTests {
     void testPlayerName() {
         player player = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
         assertThat(player.playerName, is("Florian"));
+
+    }
+
+    @Test
+    @DisplayName("Sample test")
+    void testAvatar_name() {
+        player player = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        assertThat(player.Avatar_name, is("Grognak le barbare"));
+
     }
 
     @Test
@@ -38,6 +48,28 @@ public class UnitTests {
 
         p.addMoney(20);
         assertEquals(120, p.money);
+    }
+
+    @Test
+    @DisplayName("test de recuperation de niveau")
+    void testRetrieveLevel(){
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        assertEquals(1, p.retrieveLevel());
+
+        UpdatePlayer.addXp(p,11);
+        assertEquals(2, p.retrieveLevel());
+
+        UpdatePlayer.addXp(p,20);
+        assertEquals(3, p.retrieveLevel());
+
+
+        UpdatePlayer.addXp(p,40);
+        assertEquals(4, p.retrieveLevel());
+
+
+        UpdatePlayer.addXp(p,100);
+        assertEquals(5, p.retrieveLevel());
+
     }
 
 
